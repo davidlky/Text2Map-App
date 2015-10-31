5d0c2253c550ce5b9a9fc8c24af810d14c8dfb2b
David Liu <info@mrdavidliu.me>
2015-10-31 19:07:15 -0400

    updated gitignore

---
+++ app/src/main/java/com/bostonhacks/text2map/SearchActivity.java
@@ -0,0 +1,48 @@
+package com.bostonhacks.text2map;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        findViewById(R.id.card_view_2).setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void next(View view) {
+        TextView tv  = (TextView) findViewById(R.id.text_question);
+        tv.setText("Where do you plan to go?");
+        findViewById(R.id.card_view).setVisibility(View.GONE);
+        findViewById(R.id.card_view_2).setVisibility(View.VISIBLE);
+
+    }
+}
